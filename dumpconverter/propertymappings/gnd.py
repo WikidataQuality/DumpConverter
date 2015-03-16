26d15b9bbe0333d95f496b94c6b3c98bd5399569
Dominic Sauer <dominic.sauer@student.hpi.de>
2015-03-16 11:01:53 +0100

    fix property mapping

--- dumpconverter/propertymappings/gnd.py
+++ dumpconverter/propertymappings/gnd.py
@@ -1,41 +1,41 @@
 property_mapping = {
     19: {
-        "nodeSelector": "ns:datafield[@tag='551' and ns:subfield[@code='i']='Geburtsort']/ns:subfield[@code='a']/text()"
+        "nodeSelector": ["ns:datafield[@tag='551' and ns:subfield[@code='i']='Geburtsort']/ns:subfield[@code='a']/text()"]
         },
     20: {
-        "nodeSelector": "ns:datafield[@tag='551' and ns:subfield[@code='i']='Sterbeort']/ns:subfield[@code='a']/text()"
+        "nodeSelector": ["ns:datafield[@tag='551' and ns:subfield[@code='i']='Sterbeort']/ns:subfield[@code='a']/text()"]
         },
     22: {
-        "nodeSelector": "ns:datafield[@tag='500' and ns:subfield[@code='9']='v:Vater']/ns:subfield[@code='a']",
+        "nodeSelector": ["ns:datafield[@tag='500' and ns:subfield[@code='9']='v:Vater']/ns:subfield[@code='a']"],
         "valueFormatter": "concat(substring-after(./text(), ', '), ' ', substring-before(./text(), ', '))"
         },
     25: {
-        "nodeSelector": "ns:datafield[@tag='500' and ns:subfield[@code='9']='v:Mutter']/ns:subfield[@code='a']",
+        "nodeSelector": ["ns:datafield[@tag='500' and ns:subfield[@code='9']='v:Mutter']/ns:subfield[@code='a']"],
         "valueFormatter": "concat(substring-after(./text(), ', '), ' ', substring-before(./text(), ', '))"
         },
     26: {
-        "nodeSelector": "ns:datafield[@tag='500' and ns:subfield[@code='9']='v:Ehemann' or ns:subfield[@code='9']='v:Ehefrau']/ns:subfield[@code='a']",
+        "nodeSelector": ["ns:datafield[@tag='500' and ns:subfield[@code='9']='v:Ehemann' or ns:subfield[@code='9']='v:Ehefrau']/ns:subfield[@code='a']"],
         "valueFormatter": "concat(substring-after(./text(), ', '), ' ', substring-before(./text(), ', '))"
         },
     39: {
-        "nodeSelector": "/record/datafield[@tag='550' and subfield[@code='i']='Funktion']/subfield[@code='a']/text()"
+        "nodeSelector": ["/record/datafield[@tag='550' and subfield[@code='i']='Funktion']/subfield[@code='a']/text()"]
         },
     40: {
-        "nodeSelector": "ns:datafield[@tag='500' and ns:subfield[@code='9']='v:Sohn' or ns:subfield[@code='9']='v:Tochter']/ns:subfield[@code='a']",
+        "nodeSelector": ["ns:datafield[@tag='500' and ns:subfield[@code='9']='v:Sohn' or ns:subfield[@code='9']='v:Tochter']/ns:subfield[@code='a']"],
         "valueFormatter": "concat(substring-after(./text(), ', '), ' ', substring-before(./text(), ', '))"
         },
     106: {
-         "nodeSelector": "/record/datafield[@tag='550' and (subfield[@code='i']='Charakteristischer Beruf' or subfield[@code='i']='Beruf')]/subfield[@code='a']/text()"
+         "nodeSelector": ["/record/datafield[@tag='550' and (subfield[@code='i']='Charakteristischer Beruf' or subfield[@code='i']='Beruf')]/subfield[@code='a']/text()"]
         },
     410: {
-        "nodeSelector": "/record/datafield[@tag='550' and subfield[@code='i']='Funktion']/subfield[@code='a']/text()"
+        "nodeSelector": ["/record/datafield[@tag='550' and subfield[@code='i']='Funktion']/subfield[@code='a']/text()"]
         },
     569: {
-        "nodeSelector": "ns:datafield[@tag='548' and ns:subfield[@code='i']='Exakte Lebensdaten']/ns:subfield[@code='a']",
+        "nodeSelector": ["ns:datafield[@tag='548' and ns:subfield[@code='i']='Exakte Lebensdaten']/ns:subfield[@code='a']"],
         "valueFormatter": "substring-before(./text(), '-')"
         },
     570: {
-        "nodeSelector": "ns:datafield[@tag='548' and ns:subfield[@code='i']='Exakte Lebensdaten']/ns:subfield[@code='a']",
+        "nodeSelector": ["ns:datafield[@tag='548' and ns:subfield[@code='i']='Exakte Lebensdaten']/ns:subfield[@code='a']"],
         "valueFormatter": "substring-after(./text(), '-')"
         },
     625: {
@@ -43,7 +43,7 @@ property_mapping = {
                          "ns:datafield[@tag='034' and ns:subfield[@code='9']='A:dgx']/ns:subfield[@code='f']/text()"]
         },
     1477: {
-        "nodeSelector": "ns:datafield[@tag='400' and ns:subfield[@code='i']='Wirklicher Name']/ns:subfield[@code='a']",
+        "nodeSelector": ["ns:datafield[@tag='400' and ns:subfield[@code='i']='Wirklicher Name']/ns:subfield[@code='a']"],
         "valueFormatter": "concat(substring-after(./text(), ', '), ' ', substring-before(./text(), ', '))"
     }
 }
