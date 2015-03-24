53578a7a60b7c733c291497ebd6dc22cc379a155
tamslo <tamaraslosarek@gmail.com>
2015-03-24 16:45:06 +0100

    Fixed test_dump_converter

--- dumpconverter/tests/test_dump_converter.py
+++ dumpconverter/tests/test_dump_converter.py
@@ -11,42 +11,46 @@ from dumpconverter.DumpConverter import DumpConverter
 class DumpConverterTest(TestCase):
     def setUp(self):
         # Open csv files
+        TestCase.setUp(self)
         self.csv_entities_file = open("entities.csv", "wb")
         self.csv_meta_file = open("meta.csv", "wb")
+        self.testdata = (
+            (
+                "nodes[0].split('-')[0]",
+                ["01.01.2015-31.12.2015"],
+                "01.01.2015"
+            ),
+            (
+                "nodes[1] + ' ' + nodes[0]",
+                ["Adams", "Douglas"],
+                "Douglas Adams"
+            ),
+            (
+                "nodes[0] + nodes[1]",
+                ["foo", "bar"],
+                "foobar"
+            ),
+            (
+                "nodes[1]",
+                ["foobar"],
+                None
+            )
+        )
 
     def tearDown(self):
         # Close csv files
         self.csv_entities_file.close()
         self.csv_meta_file.close()
+        TestCase.tearDown(self)
 
-    run_formatter_data_provider = (
-        (
-            "nodes[0].split('-')[0]",
-            ["01.01.2015-31.12.2015"],
-            "01.01.2015"
-        ),
-        (
-            "nodes[1] + ',' + nodes[0]",
-            ["Adams, Douglas"],
-            "Douglas Adams"
-        ),
-        (
-            "nodes[0] + nodes[1]",
-            ["foo", "bar"],
-            "foobar"
-        ),
-        (
-            "nodes[1]",
-            ["foobar"],
-            None
-        )
-    )
-
-    @data_provider(run_formatter_data_provider)
-    def test_run_formatter(self, formatter, nodes, expected_result):
+    def test_run_formatter(self):
         dump_converter = self.create_dump_converter()
-        actual_result = dump_converter.run_formatter(formatter, nodes)
-        self.assertThat(actual_result, Equals(expected_result))
+        for data in self.testdata:
+            formatter = data[0]
+            nodes = data[1]
+            expected_result = data[2]
+            actual_result = dump_converter.run_formatter(formatter, nodes)
+            self.assertThat(actual_result, Equals(expected_result))
 
     def create_dump_converter(self):
         dump_converter = DumpConverter(
